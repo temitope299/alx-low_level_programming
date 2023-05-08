5fc95d750c4065a3745d43f0fccf8e0550a96c83
temitope299 <tthhriftwitus@gmail.com>
2023-05-08 13:26:50 -0700

    C - File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,7 +8,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stddef.h>
-
+int create_file(const char *filename, char *text_content);
+int _strlen(char *str);
+int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
